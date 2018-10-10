3dd61a25f68e7aa5a48973fc44ce924ddca4fb0a
Sebastian <prawdzik.sebastian@gmail.com>
2018-10-10 17:28:31 +0200

    Initial commit

---
+++ java/pl/infoshare/App.java
@@ -0,0 +1,18 @@
+package pl.infoshare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name ="Sebix";
+        String lastName ="Praw";
+        String homeCity ="Suwalki";
+        byte age =26;
+        System.out.println("Moj wiek: " + age + "\n" + "Moje miasto: " + homeCity);
+
+    }
+}
